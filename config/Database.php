faf57970112689d43c9951a2d161a09b699d7625
Abdullah Al Imran <alimrandev@gmail.com>
2021-07-17 19:53:57 +0600

    Configure the database connection

---
+++ config/Database.php
@@ -0,0 +1,35 @@
+<?php 
+
+class Database{
+    //DB param
+
+    private $host       = 'localhost';
+    private $db_name    = 'myblog';
+    private $username   = 'root';
+    private $password   = '';
+    private $conn;
+
+    //DB Connect
+    public function connect(){
+        //defined conn as null
+        $this->conn = null;
+
+        //check error using try catch
+
+        try{
+
+            $this->conn = new PDO("mysql:host={$this->host};dbname={$this->db_name}", $this->username, $this->password);
+
+            $this->conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+        }catch(PDOException $e){
+            echo 'Connection Error ' .$e->getMessage();
+        }
+
+        return $this->conn;
+
+    }
+}
+
+$db = new Database();
+$db->connect();
